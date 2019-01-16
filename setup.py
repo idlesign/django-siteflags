8f5e127752afe909775b5c41532736fe76e5d3be
idle sign <idlesign@yandex.ru>
2019-01-16 18:20:05 +0700

    Dropped QA for Python 3.4.

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     test_suite='tests',
     tests_require=[
         'pytest',
-        'pytest-djangoapp>=0.4.1',
+        'pytest-djangoapp>=0.9.0',
     ],
 
     classifiers=[
@@ -56,7 +56,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: BSD License'
